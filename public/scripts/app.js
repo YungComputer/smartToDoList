54ff31e86b3ebe4d1bbf748cfaf40dea3bd4b1c1
ladan <pishili@users.noreply.github.com>
2020-02-10 22:23:45 -0800

    remove duplicate submit onclick

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -18,29 +18,6 @@ $(document).ready(function () {
   // loadToDo(); // this is to auto populate data from our DB for the starting page.
 
   // when form gets submitted this should run.
-
-  const $form = $('form');
-  $form.on('submit', (event) => {  // look for the element Clare uses
-    event.preventDefault();
-    const textField = $('textarea').val();
-    console.log(textField);
-
-
-    $.ajax({
-      url: '/todos',
-      method: 'POST',
-      data: textField,
-    })
-      .done((post) => {
-        $('textarea').val('') // figure out the element Clare uses, this emptys the textarea
-
-      })
-      .fail((err) => {
-        console.log(err);
-      })
-  });
-
-
   $("#submit-btn").on("click", event => {
     // look for the element Clare uses
     event.preventDefault();
