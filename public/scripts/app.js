e6c0adf1ca1042c8bbae3f49effda230f5e14c10
CLARE NOLAN <clarenolan92@gmail.com>
2020-02-11 03:26:29 +0000

    fix omdb api

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,36 +4,35 @@ $(() => {
   $.ajax({
     method: "GET",
     url: "/api/users"
-  }).done((users) => {
-    console.log(users, 'this is for users')
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
+  }).done(users => {
+    console.log(users, "this is for users");
+    for (user of users) {
+      $("<div>")
+        .text(user.name)
+        .appendTo($("body"));
     }
   });
 });
 
 $(document).ready(function() {
-
   // loadToDo(); // this is to auto populate data from our DB for the starting page.
 
   // when form gets submitted this should run.
-  $('#submit-btn').on('click', (event) => {  // look for the element Clare uses
+  $("#submit-btn").on("click", event => {
+    // look for the element Clare uses
     event.preventDefault();
     console.log("HERE");
-    const textField = $('textarea').val();
-      $.ajax({
-        url: '/todos',
-        method: 'POST',
-        data: $('#task-form').serialize(),
+    const textField = $("textarea").val();
+    $.ajax({
+      url: "/todos",
+      method: "POST",
+      data: $("#task-form").serialize()
+    })
+      .done(post => {
+        $("textarea").val(""); // empties the text area
       })
-        .done((post) => {
-          $('textarea').val('') // figure out the element Clare uses, this emptys the textarea
-
-        })
-        .fail((err) => {
-          console.log(err);
-        })
-
+      .fail(err => {
+        console.log(err);
+      });
   });
-
 });
